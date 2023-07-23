445a703bf9e73d344f3288a7ec3c4fac3e56af56
CaptainCompileon <omgado@gmail.com>
2023-07-23 12:02:44 +0200

    add basic layout

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <title>Adam Samko Resume</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="description" content="Adam Samko Resume" />
+      </Head>
       <body>
-        <Main />
+      <Main />
         <NextScript />
       </body>
     </Html>
